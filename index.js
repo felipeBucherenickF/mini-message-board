a8af1bcd0af349fd866e6c7d17797a0bc2d0e33a
Felipe Bucherenick <felipebucherenick@gmail.com>
2025-07-03 19:56:01 -0400

    feat: Add the new endpoint

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const messages = [
 
 const express = require("express");
 const app = express();
+const messageRouter = express.Router();
 const path = require("node:path");
 const PORT = 3000;
 
@@ -26,9 +27,26 @@ app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+app.use(messageRouter);
+
+messageRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+messageRouter.post("/new", (req, res) => {
+  const { messageUser } = req.body;
+  const { messageText } = req.body;
+  messages.push({
+    text: messageText,
+    name: messageUser,
+    date: new Date().toLocaleDateString(),
+  });
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
---
+++ routers/messageRouter.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/new", (req, res) => {});
+
+module.exports = router;

99e525042be54e30a02a527e7b95fae7073c9bdf
Felipe Bucherenick <felipebucherenick@gmail.com>
2025-07-03 19:26:44 -0400

    feat: Showing de messages at index

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Armando",
+    added: new Date().toLocaleDateString(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleDateString(),
+  },
+  {
+    text: "Wenaaaaaaaaaaaaaaa!!",
+    user: "Rony",
+    added: new Date().toLocaleDateString(),
+  },
+];
+
 const express = require("express");
-const messageRouter = require("./routers/messageRouter");
 const app = express();
+const path = require("node:path");
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Mini Message Board");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use("/messages", messageRouter);
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
 
 app.listen(PORT, () => {
   console.log("App Listen in port: 3000");
--- routers/messageRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-
-const messageRouter = Router();
-const { getMessages } = require("../controllers/getMessages");
-
-messageRouter.get("/", getMessages);
-
-module.exports = messageRouter;
